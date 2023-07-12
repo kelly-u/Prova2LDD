d381b7edf66a8562f4f50670aea47e18567e33ab
G. Kelly <38537574+kelly-u@users.noreply.github.com>
2023-07-12 14:25:30 -0300

    Trazendo a chave e o valor corretamente
    Agora já está somando a quantidade de álbuns lançados no ano + ano.

--- Q2Dom.java
+++ Q2Dom.java
@@ -1,8 +1,8 @@
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,35 +15,65 @@ import org.xml.sax.SAXException;
  * @author G. Kelly
  */
 
-/* Em qual ANO o artista lançou mais ÁLBUNS
-e QUANTOS ÁLBUNS foram lançados?
-Vai ter que ter uma estrutura que vai associar ANOS a quantidade de ÁLBUNS
-Depois mandar ordenar.
-O que tiver mais álbuns, devem ser impressos
-
-Na prova é qual TIME teve mais PONTOS (só faltou QNTS pontos)
-Vai ter que ter uma estrutura que vai associar TIMES a quantidade de PONTOS
-Depois mandar ordenar.
-O que tiver mais pontos, devem ser impressos
-
-Saída esperada:
-< year count ="3"> 1972 </ year >*/
-
 public class Q2Dom {
-    
+
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.parse(new File("src/CaetanoVeloso.xml"));
-        // Um cara, crachá. A string ANO vai estar associada a um integer QNT DE ÁLBUNS NO ANO (String)
-        Map<String, Integer> mapa = new HashMap();
-          
-        // Precisa saber o nome dos times (elementos)
-        // EU preciso saber o ano dos jogos (atributos)
+        List<Ano> mapa = new ArrayList();
+        
         NodeList album = doc.getElementsByTagName("album");
         for (int i = 0; i < album.getLength(); i++) {
-            Element a1 = (Element) album.item(i);
-            System.out.println(a1.getAttribute("year"));
+            Element alb1 = (Element) album.item(i);
+            String ano = alb1.getAttribute("year");
+
+            Ano ano1 = mapa.stream().filter(a -> a.getNome().equals(ano)).findFirst().orElse(null);
+            if (ano1 != null) {
+                int qG = ano1.getaGRav();
+                qG += 1;
+                ano1.setaGRav(qG);
+
+            } else {
+                Ano temp = new Ano(ano, 1);
+                int p1 = temp.getaGRav();
+                temp.setaGRav(p1);
+                mapa.add(temp);
+            }
         }
+        System.out.println(mapa);
     }
+}
+
+class Ano {
+
+    String nome;
+    int aGRav;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getaGRav() {
+        return aGRav;
+    }
+
+    public void setaGRav(int aGRav) {
+        this.aGRav = aGRav;
+    }
+
+    public Ano(String nome, int aGRav) {
+        this.nome = nome;
+        this.aGRav = aGRav;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %d", nome, aGRav);
+    }
+
 }
